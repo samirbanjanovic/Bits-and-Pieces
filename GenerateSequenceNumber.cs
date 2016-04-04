4e71d22a7f93c9846d458b01cefd131c752cc68e
Samir Banjanovic <banjoCoder@users.noreply.github.com>
2016-04-04 11:03:47 -0400

    Added method for generating sequence number

---
+++ GenerateSequenceNumber.cs
@@ -0,0 +1,24 @@
+private static string GenerateSequenceNumber()
+{
+    var guid = Guid.NewGuid().ToString("N");
+    var ca = Encoding.Default.GetBytes(guid);
+    var csa = string.Join("", ca);
+
+    double byteGuid;
+    double.TryParse(csa, out byteGuid);
+
+    byteGuid = byteGuid % DateTime.Now.ToOADate();
+
+    var sequence = byteGuid.ToString().Replace(".", "");
+
+    if (sequence.Length > 11)
+    {
+        sequence = sequence.Substring(0, 11);
+    }
+    else
+    {
+        sequence = sequence.PadLeft(11, '0');
+    }
+
+    return sequence;
+}
